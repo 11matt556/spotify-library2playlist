78198e2086e1227950ffce52ea9ff972ac4c63e3
Matthew Howell <llmatt556@hotmail.com>
2018-06-15 17:20:40 -0400

    Trying to figure out spotify auth

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,4 @@
 const spotifyApi = new SpotifyWebApi()
-// Get Elvis' albums
-spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
-  .then(function(data) {
-    console.log('Artist albums', data.headers);
-  }, function(err) {
-    console.error(err);
-});
-
 //jQuery.get('https://accounts.spotify.com/authorize/?client_id=b1aa5dbffa494726880c00c395523fe6&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&response_type=code')
 /*
 async function getAuth() {
@@ -17,7 +9,7 @@ async function getAuth() {
     return result;
 }*/
 
-//Implicit Gran
+//Implicit Grant
 
 let token = jQuery.param('access_token');
 console.log(token);
@@ -27,8 +19,16 @@ window.location = "https://accounts.spotify.com/authorize?client_id=b1aa5dbffa49
 }
 else{
     console.log(token + " success")
+    spotifyApi.setAccessToken(token)
 }
 
+// Get Elvis' albums
+spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
+  .then(function(data) {
+    console.log('Artist albums', data.headers);
+  }, function(err) {
+    console.error(err);
+});
 
 spotifyApi.getUserPlaylists()  // note that we don't pass a user id
   .then(function(data) {
