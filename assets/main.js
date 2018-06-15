80d7e08204bdc52e92e4b9adf95067c167698568
Matthew Howell <llmatt556@hotmail.com>
2018-06-15 17:24:12 -0400

    Trying to figure out spotify auth

--- assets/main.js
+++ assets/main.js
@@ -11,11 +11,10 @@ async function getAuth() {
 
 //Implicit Grant
 
-let token = jQuery.param('access_token');
-console.log(token);
+let token = spotifyApi.getAccessToken();
 
-if(token==""){
-//window.location = "https://accounts.spotify.com/authorize?client_id=b1aa5dbffa494726880c00c395523fe6&redirect_uri=https://howell-info.us/projects/spotify-library2playlist/index.html&response_type=token"
+if(token == ""){
+window.location = "https://accounts.spotify.com/authorize?client_id=b1aa5dbffa494726880c00c395523fe6&redirect_uri=https://howell-info.us/projects/spotify-library2playlist/index.html&response_type=token"
 }
 else{
     console.log(token + " success")
