884bc2c69760540769484c6bef7e30eda8f9d58b
Matthew Howell <llmatt556@hotmail.com>
2018-06-15 17:35:35 -0400

    Trying to figure out spotify auth

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,10 @@
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
 const spotifyApi = new SpotifyWebApi()
 //jQuery.get('https://accounts.spotify.com/authorize/?client_id=b1aa5dbffa494726880c00c395523fe6&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&response_type=code')
 /*
@@ -9,12 +16,15 @@ async function getAuth() {
     return result;
 }*/
 
+
+//https://accounts.spotify.com/en/authorize?client_id=b1aa5dbffa494726880c00c395523fe6&redirect_uri=https:%2F%2Fhowell-info.us%2Fprojects%2Fspotify-library2playlist%2Findex.html&response_type=token
+
 //Implicit Grant
 spotifyApi.setAccessToken('');
 let token = spotifyApi.getAccessToken();
 console.log(token);
 
-token = jQuery.param('access_token');
+token = getUrlParameter('access_token')
 console.log(token + " success")
 spotifyApi.setAccessToken(token)
 
