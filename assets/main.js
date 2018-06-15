b1d73b0ffc7c12628542aeca67a8c165e52fcbbf
Matthew Howell <llmatt556@hotmail.com>
2018-06-15 17:27:55 -0400

    Trying to figure out spotify auth

--- assets/main.js
+++ assets/main.js
@@ -14,14 +14,9 @@ spotifyApi.setAccessToken('');
 let token = spotifyApi.getAccessToken();
 console.log(token);
 
-if(token == ""){
-window.location = "https://accounts.spotify.com/authorize?client_id=b1aa5dbffa494726880c00c395523fe6&redirect_uri=https://howell-info.us/projects/spotify-library2playlist/index.html&response_type=token"
-}
-else{
-    token = jQuery.param('access_token');
-    console.log(token + " success")
-    spotifyApi.setAccessToken(token)
-}
+token = jQuery.param('access_token');
+console.log(token + " success")
+spotifyApi.setAccessToken(token)
 
 // Get Elvis' albums
 spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
