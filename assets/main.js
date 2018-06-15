2e05ac7587c61b102d3bfb5df5e852b767ecb6cb
Matthew Howell <llmatt556@hotmail.com>
2018-06-15 17:52:21 -0400

    Auth works but no error handling or anything yet.

--- assets/main.js
+++ assets/main.js
@@ -1,42 +1,16 @@
-function getUrlParameter(name) {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-    var results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-};
+/*
+//Implicit Grant
+User muist visit this link to authorize:
+https://accounts.spotify.com/en/authorize?client_id=b1aa5dbffa494726880c00c395523fe6&redirect_uri=https:%2F%2Fhowell-info.us%2Fprojects%2Fspotify-library2playlist%2Findex.html&response_type=token
+
+*/
 
 const spotifyApi = new SpotifyWebApi()
-//jQuery.get('https://accounts.spotify.com/authorize/?client_id=b1aa5dbffa494726880c00c395523fe6&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&response_type=code')
-/*
-async function getAuth() {
-    let result = await jQuery.ajax({
-        method: "GET",
-        url: "https://accounts.spotify.com/authorize/?client_id=b1aa5dbffa494726880c00c395523fe6&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&response_type=code",
-    })
-    return result;
-}*/
 
-
-//https://accounts.spotify.com/en/authorize?client_id=b1aa5dbffa494726880c00c395523fe6&redirect_uri=https:%2F%2Fhowell-info.us%2Fprojects%2Fspotify-library2playlist%2Findex.html&response_type=token
-
-//Implicit Grant
-spotifyApi.setAccessToken('');
-let token = spotifyApi.getAccessToken();
-console.log(token);
-
-//token = getUrlParameter('access_token')
 token = window.location.hash
-token = token.substring(14)
+token = token.substring(14) //Trim off "#access_token="
 console.log(token + " success")
 spotifyApi.setAccessToken(token)
-
-// Get Elvis' albums
-spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
-  .then(function(data) {
-    console.log('Artist albums', data.headers);
-  }, function(err) {
-    console.error(err);
-});
 
 spotifyApi.getUserPlaylists()  // note that we don't pass a user id
   .then(function(data) {
