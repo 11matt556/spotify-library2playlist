2103792b1d95998b06d279412a176084ce138c6b
Matthew Howell <llmatt556@hotmail.com>
2018-06-15 16:56:21 -0400

    Trying to figure out spotify auth

--- assets/main.js
+++ assets/main.js
@@ -13,14 +13,18 @@ spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
 });
 
 //jQuery.get('https://accounts.spotify.com/authorize/?client_id=b1aa5dbffa494726880c00c395523fe6&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&response_type=code')
-
+/*
 async function getAuth() {
     let result = await jQuery.ajax({
         method: "GET",
         url: "https://accounts.spotify.com/authorize/?client_id=b1aa5dbffa494726880c00c395523fe6&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&response_type=code",
     })
     return result;
-}
+}*/
+
+window.location = "https://accounts.spotify.com/authorize?client_id=b1aa5dbffa494726880c00c395523fe6&redirect_uri=file%3A%2F%2F%2FZ%3A%2FDocuments%2FPersonal%2520Programming%2520Projects%2Fspotify-library2playlist%2Findex.html&response_type=token"
+
+//Implicit Grant
 
 spotifyApi.getUserPlaylists()  // note that we don't pass a user id
   .then(function(data) {
