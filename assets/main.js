c58da25dc534c0782c98c914f16b1f6a9030ebfc
Matthew Howell <llmatt556@hotmail.com>
2018-06-18 12:26:13 -0400

    Doing promises

--- assets/main.js
+++ assets/main.js
@@ -18,27 +18,6 @@ if (token) {
 
 let offset = 0;
 
-async function getSavedTracksWrapper(limit, offset) {
-    let result = spotifyApi.getMySavedTracks({
-            "limit": limit,
-            "offset": offset
-        })
-        .then(function (data) {
-            console.log('User Tracks', data);
-            if ((offset + limit) < (data.total)) {
-                getSavedTracksWrapper(limit, (offset + limit))
-            } else {
-                //Finished loading tracks
-
-            }
-
-        }, function (err) {
-            console.error(err);
-        });
-
-    return result;
-}
-
 async function getSavedTrack(limit, offset) {
     
     let result = await spotifyApi.getMySavedTracks({
@@ -50,35 +29,24 @@ async function getSavedTrack(limit, offset) {
 }
 
 async function getUserIDWrapper() {
-    var userID = null
-    let result = spotifyApi.getMe().then(function (data) {
-        console.log('User Data', data);
-        userID = data.id;
-        console.log('UserID', userID);
-    }, function (err) {
-        console.error(err)
-    })
-
+    let result = await spotifyApi.getMe()
     return result;
 }
 
-
-
-async function getUserInformationAndLibrary() {
-    //let result1 = await getSavedTracksWrapper(50, 0); //TODO: Currently cannot determine when tracks are finished being retrieved. Convert to for loop?
-    let result1 = await getSavedTracks(50, 0)
-    let result2 = await getUserIDWrapper();
-    return result2;
+async function getSavedTracks(limit, offset, total){
+    var promises = [];
+    
+    while((offset + limit) < total){
+        offset = offset + limit;
+        var res = await spotifyApi.getMySavedTracks({"limit":limit,"offset":offset})
+        promises.push(res);
+    }
+    
+    return Promise.all(promises);
 }
 
-
-/*
-getUserInformationAndLibrary().then(function () {
-    console.log("Did the things");
-});
-*/
-
 getSavedTrack(50,0).then(function(data){
-    console.log("Did the things");
     console.log(data);
+    var moreData = getSavedTracks(50,data.offset,data.limit);
+    console.log(moreData);
 })
